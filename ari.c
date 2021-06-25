ca444d228f5b17677d364ca40accd68cd9b69965
Gopikant kumar <76965991+Gopi-2001@users.noreply.github.com>
2021-06-25 16:23:26 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -3,6 +3,7 @@ int num_letter = letters(s);
 int num_words = words(s);
 int num_sentences = sentences(s);
 
-int value = 4.71*((double)num_letters/(double)num_words)
+int value = 4.71*((double)num_letters/(double)num_words) + 0.5*((double)num_words/(double)num_sentences) -21.43;
+  
 
 }
